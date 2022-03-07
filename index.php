fc5e886feda36e87863155c6edf1a7681d2bddcf
felicity12345 <f.osawaru@rgu.ac.uk>
2022-03-06 17:53:00 -0800

    yes

--- index.php
+++ index.php
@@ -1,29 +1,6 @@
 <?php 
   include_once 'header.php';
   ?>
-        
-         <section class="index-intro">
-          <h1>School Gradding App Introduction</h1>
-          <p> purpose of the Gradding Application</p>
-        </section>
-
-        <section class="index-about">
-            <h2>some basic categories</h2>
-            <div class="index-about-list">
-            <div>
-                <h3>Games</h3>
-            </div>
-            <div>
-                <h3>Competiton</h3>
-            </div>
-            <div>
-                <h3>Sport</h3>
-            </div>
-            <div>
-                <h3>Break</h3>
-            </div>
-        </div>
-    </section>
     <?php 
   include_once 'footer.php';
   ?>
