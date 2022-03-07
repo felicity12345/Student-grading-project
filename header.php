0362606311e955ac963a1ceb0d2ce4abaa13af3d
felicity12345 <f.osawaru@rgu.ac.uk>
2022-03-06 18:28:11 -0800

    yes

--- header.php
+++ header.php
@@ -14,8 +14,8 @@
             <ul>
                 <li><a href="index.php">Home</a></li>
                 <li><a href="about.php">About Us</a></li>
+                <li><a href="apearlregister.php">Sign up</a></li>
                 <li><a href="apearllogin.php">Log in</a></li>
-               
 
             </ul>
 
