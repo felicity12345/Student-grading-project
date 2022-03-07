1503c89a9e9b31b7970b833708a81bbc71b94179
felicity12345 <f.osawaru@rgu.ac.uk>
2022-03-06 19:01:19 -0800

    yes

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   ?>
         
         <header id="showcase2">
-        <h2>Welcome to Apearl Home page</h2>
+        <h2>Welcome to Aberdeen City Home page</h2>
         <p>Lorem, ipsum., ipsum.  ipsum dolor sit amet consectetur adipisicing elit. Doloribus sint cum natus, vero nam accusamus! ipsum dolor sit amet consectetur adipisicing elit. Modi accusantium qui sapiente, veniam harum, architecto tempora quod a consequatur, obcaecati minus quaerat. Obcaecati molestiae numquam a et, nesciunt minus provident eos dolorem ratione magnam vero deserunt aspernatur! Ea ad ipsum doloremque iste a nemo hic quia tempora nostrum minus. Iusto quisquam dicta ullam tempora aspernatur ipsam, architecto inventore soluta expedita laudantium. Animi nulla corrupti itaque repudiandae incidunt. Mollitia blanditiis voluptate ullam tempora amet dolorem illo, repellat similique molestias neque alias, asperiores recusandae voluptatum ipsam minima rerum ut minus. Ullam ut, quas ipsam commodi odit laudantium in distinctio iusto dolores rerum totam voluptas veritatis eius aliquam, ad asperiores facilis quia numquam?</p>
         
     </header>
